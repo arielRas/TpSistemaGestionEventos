675976ed6e95e61afb46d5fd54b0589962fd9493
arielRas <ariel.ras.90@gmail.com>
2023-12-27 20:42:04 -0300

    Se agregan campos a clases

--- Entity/Organizador.cs
+++ Entity/Organizador.cs
@@ -8,8 +8,16 @@ namespace Entity
 {
     public class Organizador : Persona
     {
+        //ATRIBUTOS
+        private string provincia;
+        private string direccion;
+        private string telefono;
         private List<Evento> eventos = new List<Evento>();
 
+        //PROPIEDADES
+        public string Provincia { get => provincia; set => provincia = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
         public List<Evento> Eventos { get => eventos; set => eventos = value; }
     }
 }
--- Entity/Proveedor.cs
+++ Entity/Proveedor.cs
@@ -10,12 +10,22 @@ namespace Entity
     {
         //ATRIBUTOS
         private float puntaje;
+        private string provincia;
+        private string direccion;
+        private string telefono;
         private List<ServicioPublicado>serviciosPublicados = new List<ServicioPublicado>();
         private List<ServicioContratado>serviciosPorCumplir = new List<ServicioContratado>();
 
         //PROPIEDADES
+        public string Provincia { get => provincia; set => provincia = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
         public float Puntaje { get => puntaje; set => puntaje = value; }
         public List<ServicioPublicado> ServiciosPublicados { get => serviciosPublicados; set => serviciosPublicados = value; }
         public List<ServicioContratado> ServiciosPorCumplir { get => serviciosPorCumplir; set => serviciosPorCumplir = value; }
     }
+    
+        
+       
+        
 }
